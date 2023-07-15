12ddd40fdf90066c83a036a18b227f0f793bda0d
etcwoo <woodonggyu92@gmail.com>
2023-07-15 19:02:58 +0900

    fix: attributes must be accessed on specific instances

--- outputs.tf
+++ outputs.tf
@@ -20,5 +20,5 @@ output "aws_wafv2_tags_all" {
 
 output "aws_wafv2_web_acl_logging_configuration_id" {
   description = "The Amazon Resource Name (ARN) of the WAFv2 Web ACL."
-  value       = aws_wafv2_web_acl_logging_configuration.this[0].id
+  value       = aws_wafv2_web_acl_logging_configuration.this[*].id
 }
