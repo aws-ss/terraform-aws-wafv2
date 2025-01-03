448add546a1f4f919e06a4c327fbcc3e90534b1c
uyggnodoow <woodonggyu92@gmail.com>
2025-01-03 13:46:01 +0900

    fix: Update terraform/providers version

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.3.6"
+  required_version = ">= 1.4.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.51.0"
+      version = ">= 5.82.2"
     }
   }
 }
