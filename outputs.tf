17fcff13c423d82bc8fb4175305faf6e22750d8f
Benjamin Priestman <benjamin.priestman@itv.com>
2025-09-19 21:41:37 +0100

    chore: add more outputs
    To aid vendoring this module into others, add more outputs so that we can test them.

--- outputs.tf
+++ outputs.tf
@@ -8,14 +8,44 @@ output "aws_wafv2_capacity" {
   value       = aws_wafv2_web_acl.this.capacity
 }
 
+output "aws_wafv2_custom_response_body" {
+  description = "The custom response body configuration of the WAF WebACL."
+  value       = aws_wafv2_web_acl.this.custom_response_body
+}
+
+output "aws_wafv2_default_action" {
+  description = "The default action of the WAF WebACL."
+  value       = aws_wafv2_web_acl.this.default_action
+}
+
 output "aws_wafv2_id" {
   description = "The ID of the WAF WebACL."
   value       = aws_wafv2_web_acl.this.id
 }
 
+output "aws_wafv2_name" {
+  description = "The name of the WAF WebACL."
+  value       = aws_wafv2_web_acl.this.name
+}
+
+output "aws_wafv2_rule" {
+  description = "The rules configuration of the WAF WebACL."
+  value       = aws_wafv2_web_acl.this.rule
+}
+
+output "aws_wafv2_scope" {
+  description = "The scope of the WAF WebACL."
+  value       = aws_wafv2_web_acl.this.scope
+}
+
 output "aws_wafv2_tags_all" {
   description = "Map of tags assigned to the resource, including those inherited from the provider default_tags configuration block."
   value       = aws_wafv2_web_acl.this.tags_all
+}
+
+output "aws_wafv2_visibility_config" {
+  description = "The visibility configuration of the WAF WebACL."
+  value       = aws_wafv2_web_acl.this.visibility_config
 }
 
 output "aws_wafv2_web_acl_logging_configuration_id" {
